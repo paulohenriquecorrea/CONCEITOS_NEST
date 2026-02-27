b812c4e02d3a4d0116b0c9cdc4a8e7cf07f5c588
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2026-02-27 17:11:35 -0400

    Finalizada aula 'Criando meu próprio Module'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
 
 @Module({
-  imports: [],
+  imports: [ConceitosManualModule],
   controllers: [AppController],
   providers: [AppService],
 })
