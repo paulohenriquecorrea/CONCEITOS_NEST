a06e5b1a9375b2bee41254a09efb4b5a7826020a
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-02-28 11:46:07 -0400

    Finalizada aula 8. Básico sobre protocolo HTTP

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('home')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello') // Método da solicitação -> Ler (Read) -> CRUD
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
