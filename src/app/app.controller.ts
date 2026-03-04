073785ed742be0fb2e6b60358b4bd122fbe7108e
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-03-03 21:46:33 -0400

    Criado arquivo client.rest

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('exemplo')
+  getExemplo(): string {
+    return 'Rota de exemplo';
+  }
 }
