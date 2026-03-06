fe36e52811a057b3dfa81251888fed30b1ab504e
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-03-05 22:09:03 -0400

    Criado o primeiro Controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
+import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
 
 @Module({
-  imports: [ConceitosManualModule],
+  imports: [ConceitosManualModule, ConceitosAutomaticoModule],
   controllers: [AppController],
   providers: [AppService],
 })
